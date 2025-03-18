16f76aaaea00a8ce689faa1dbcbbf9259a13be29
daisuke <wyfnt119@gmail.com>
2025-03-18 22:24:23 +0900

    edit config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // ここに追記する
+  base: '/pages-test-vue-01/', // ここに追記する
   plugins: [
     vue(),
     vueDevTools(),
